a26ecbacc0053b92a2f93fee24a2eea82282598f
michaschwab <me@michaschwab.de>
2018-05-14 16:10:54 -0400

    loading some random data for drawing rectangles etc

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var eachTimelineWaitTimeInMs = 1;
+
+var timelineHeight = 110;
+var numberTimelines = 15;
+var timelineWidth = 800;
+var timelineBarWidth = 5;
 
 var alldomz = document.getElementById('alldomz');
 var canvas = document.getElementById('canvaz');
@@ -8,13 +14,60 @@ canvas.style.width = canvas.width / scale + 'px';
 canvas.style.height = canvas.height / scale + 'px';
 ctx.scale(scale, scale);
 
+var drawTimeline = function(y, ctx, data)
+{
+  //ctx.strokeRect(0, y, timelineWidth, 100);
+  //ctx.fillText('Drawn on canvas', 10, y + timelineHeight / 2 - 5);
+  for(var i = 0; i < data.length; i++)
+  {
+    ctx.fillRect(i * timelineBarWidth, y, timelineBarWidth, data[i]*100);
+  }
+};
+
+
+var loadTimelines = function()
+{
+  var data = [];
+
+  for(var i = 0; i < numberTimelines; i++)
+  {
+    data.push(loadTimeline());
+  }
+  return data;
+};
+
+var loadTimeline = function()
+{
+  var data = [];
+  for(var i = 0; i < timelineWidth / timelineBarWidth; i++)
+  {
+    data.push(Math.random());
+  }
+
+  wait(eachTimelineWaitTimeInMs);
+
+  return data;
+};
+
+var wait = function(ms)
+{
+  var start = Date.now();
+  while(Date.now() - start < ms)
+  {
+
+  }
+};
+
+
+var data = loadTimelines();
+
+
 var raf = function() {
   ctx.clearRect(0, 0, 1000, 1000);
   var scrollTop = document.getElementById('scroll').scrollTop;
-  for (var i = 0; i < 15; i++) {
-    const y = i * 110 - scrollTop;
-    ctx.strokeRect(0, y, 400, 100);
-    ctx.fillText('Drawn on canvas', 10, y + 50);
+  for (var i = 0; i < data.length; i++) {
+    const y = i * timelineHeight - scrollTop;
+    drawTimeline(y, ctx, data[i]);
   }
   alldomz.style.top = '-' + scrollTop + 'px';
   requestAnimationFrame(raf);
